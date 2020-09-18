3586d50dde072d4fa56b136f433b715f0df76eff
sqhy <zhangfengbo@37.com>
2020-09-18 17:46:46 +0800

    fix(技能): 删除无用代码

--- server/src/main/java/server/cmdhandler/CmdHandlerFactory.java
+++ server/src/main/java/server/cmdhandler/CmdHandlerFactory.java
@@ -27,8 +27,6 @@ public class CmdHandlerFactory {
     private static final Map<Class<?>, ICmdHandler<? extends GeneratedMessageV3>> HANDLER_MAP = new HashMap<>();
 
 
-    private static final Map<Class<?>,ISkillHandler<? extends AbstractSkillProperty>> SKILL_HANDLER_MAP = new HashMap<>();
-
     private CmdHandlerFactory() {
     }
 
@@ -78,54 +76,7 @@ public class CmdHandlerFactory {
             }
         }
 
-        initSkillHandler();
     }
-
-
-    public static void initSkillHandler(){
-        log.info("=== 完成 SkillCmd 和 Handler 的关联! ===");
-        // 获取 Handler 类
-        Set<Class<?>> listSubClazz = PackageUtil.listSubClazz(
-                ISkillHandler.class.getPackage().getName(),
-                true,
-                ISkillHandler.class
-        );
-        for (Class<?> subClazz : listSubClazz) {
-            // 判断是否是抽象类
-            if (Modifier.isAbstract(subClazz.getModifiers())) {
-                continue;
-            }
-            // 获取方法
-            Method[] clazzMethods = subClazz.getMethods();
-            Class<?> msgType = null;
-            for (Method clazzMethod : clazzMethods) {
-
-                if (!"skillHandle".equals(clazzMethod.getName())) {
-                    continue;
-                }
-                Class<?>[] parameterTypes = clazzMethod.getParameterTypes();
-                if (parameterTypes.length < 2 ||
-                        parameterTypes[1] == AbstractSkillProperty.class ||
-                        !AbstractSkillProperty.class.isAssignableFrom(parameterTypes[1])) {
-                    continue;
-                }
-                msgType = parameterTypes[1];
-                break;
-            }
-            if (msgType == null) {
-                continue;
-            }
-            //创建 消息处理类
-            try {
-                ISkillHandler<?> cmdHandler = (ISkillHandler<?>) GameServer.APPLICATION_CONTEXT.getBean(subClazz);
-                log.info("关联 {} <==> {} ", msgType.getName(), subClazz.getName());
-                SKILL_HANDLER_MAP.put(msgType, cmdHandler);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
 
 
     /**
@@ -141,16 +92,5 @@ public class CmdHandlerFactory {
         return HANDLER_MAP.get(clazz);
     }
 
-    /**
-     * 通过技能消息类型获得对应的处理类
-     *
-     * @param clazz
-     * @return
-     */
-    public static ISkillHandler<? extends AbstractSkillProperty> getSkillHandlerByClazz(Class<?> clazz) {
-        if (clazz == null) {
-            return null;
-        }
-        return SKILL_HANDLER_MAP.get(clazz);
-    }
+
 }
